c522305553b52349c3a15692513828a00fb99d32
Konrad Ciejek <konrad.ciejek@gmail.com>
2024-11-19 18:34:01 +0100

    refactored calculated quotes

--- src/main/java/com/algoteque/system/CourseController.java
+++ src/main/java/com/algoteque/system/CourseController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,13 +17,11 @@ public class CourseController {
     private final CourseService courseService;
 
     @PostMapping
-    public List<Quote> sendTeacherRequest(@RequestBody TeacherRequest teacherRequest) throws IOException {
+    public List<Quote> sendTeacherRequest(@RequestBody TeacherRequest teacherRequest) {
         final Map<String, Double> quotesPerProvider = courseService.getQuotesPerProvider(teacherRequest);
-        List<Quote> quotes = new ArrayList<>();
-        for (Map.Entry<String, Double> entry : quotesPerProvider.entrySet()) {
-            quotes.add(new Quote(entry.getKey(), entry.getValue()));
-        }
 
-        return quotes;
+        return quotesPerProvider.entrySet().stream()
+                .map(entry -> new Quote(entry.getKey(), entry.getValue()))
+                .toList();
     }
 }
